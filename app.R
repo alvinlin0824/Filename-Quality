2dcaf85a30101bd3c0f36183c55e6c660274d1b6
alvinlin0824 <alvin.lin@abbott.com>
2024-02-15 10:48:37 -0800

    Commit From RStudio

--- app.R
+++ app.R
@@ -4,32 +4,81 @@ library(tidyverse)
 library(reactable)
 library(fs)
 
+# InHouse
 t_drive <- gsub("\\\\", "/", r"(\\oneabbott.com\dept\ADC\Technical_OPS\Clinical_Affairs\CDM_23238\)")
-# study event
-event <- dir_ls(gsub("\\\\", "/", r"(\\wf00168p.oneabbott.com\data1\CDM\ADC-US-RES-23238\OpenClinicaExtract\Current)")) |>
-         str_extract("(?<=Current/).{3}")
+
+# External
+m_drive <- gsub("\\\\", "/", r"(\\wf00168p.oneabbott.com\data1\CDM\)")
+
+
+study_list <- dir_ls(gsub("\\\\", "/", r"(\\wf00168p.oneabbott.com\data1\CDM)"),type = "directory") |>
+              file_info() |>
+              # Filter Year >= 2022
+              filter(year(modification_time) >= 2022) |>
+              # Filter Study
+              filter(str_detect(path,"RES|PMS|VAL|EXP")) |>
+              # Length
+              mutate(Path = str_extract(path, regex("(?<=CDM/).+"))) |> 
+              filter(str_length(Path) == 16) |> 
+              pull(Path)
+              
 
 ui <- dashboardPage(
         dashboardHeader(title = "Filename Quality"),
-        dashboardSidebar(selectInput("event", "Select Event Number", event),
-                         textInput("event1","Input Event Number")
+        dashboardSidebar(selectInput("study", "Please Select Study", study_list,width = "300px"),
+                         uiOutput("new")
+                         # textInput("event1","Input Event Number")
                          ),
         dashboardBody(reactableOutput("table"),waiter::use_waiter())
 )
 
 server <- function(input, output) {
+  
+  # study event
+  # Inhouse
+  output$new <- renderUI({
+    if (input$study %in% c("ADC-US-RES-23238")){
+       # InHouse
+       event <- dir_ls(gsub("\\\\", "/", r"(\\wf00168p.oneabbott.com\data1\CDM\ADC-US-RES-23238\OpenClinicaExtract\Current)")) |>
+                str_extract("(?<=Current/).{3}")
+       selectInput("event", h6("Please select study event"), event, width = "400px")
+    } else if (input$study %in% c("ADC-US-RES-22225")) {
+      event <- dir_ls(gsub("\\\\", "/", r"(\\wf00168p.oneabbott.com\data1\CDM\ADC-US-RES-22225\)"), type = "directory", regexp = "SE") |>
+               str_extract("(?<=[:digit:]{5}/).+")
+      selectInput("event", h6("Please select study event"), event, width = "400px")
+    } else if (input$study %in% c("ADC-US-RES-21211")) {
+      event <- dir_ls(gsub("\\\\", "/", r"(\\wf00168p.oneabbott.com\data1\CDM\ADC-US-RES-21211\)"), type = "directory", regexp = "SE") |>
+               str_extract("(?<=[:digit:]{5}/).+")
+      selectInput("event", h6("Please select study event"), event, width = "400px")
+    } else if (input$study %in% c("ADC-US-RES-23241")) {
+      event <- dir_ls(gsub("\\\\", "/", r"(\\wf00168p.oneabbott.com\data1\CDM\ADC-US-RES-23241\)"), type = "directory", regexp = "SE") |>
+               str_extract("(?<=[:digit:]{5}/).+")
+      selectInput("event", h6("Please select study event"), event, width = "400px")
+    }
+      else {
+         return(NULL)
+    }
+  })
 
+  # \\wf00168p.oneabbott.com\data1\CDM\ADC-US-RES-22225\SE23_24_Glu\UploadData
+  # \\wf00168p.oneabbott.com\data1\CDM\ADC-US-VAL-21216\UploadData\AUU
   # Directory
   text <- reactive({
-          req(input$event)
+          req(input$study,input$event)
             waiter <- waiter::Waiter$new()
             waiter$show()
             on.exit(waiter$hide())
             # Sys.sleep(sample(5, 1))
             # runif(1)
+          if (input$study %in% c("ADC-US-RES-23238")) {
+               file_list <- dir_ls(str_c(t_drive,input$event), recurse = T, glob = "*extracted_realm.zip|*events.csv|*devices.csv")
+          } else if (input$study %in% c("ADC-US-RES-22225","ADC-US-RES-21211","ADC-US-RES-21211","ADC-US-RES-23241")) {
+               file_list <- dir_ls(str_c(m_drive,input$study,"/",input$event), recurse = T, glob = "*extracted_realm.zip|*events.csv|*devices.csv")
+          } else {
+               file_list <- dir_ls(str_c(m_drive,input$study), recurse = T, glob = "*extracted_realm.zip|*events.csv|*devices.csv")
+          }
           
-          file_list <- dir_ls(str_c(t_drive,input$event),recurse = T,glob = "*extracted_realm.zip|*events.csv|*devices.csv")
-          # Filter Path
+            # Filter Path
           if (any(str_detect(file_list,"CDM_23238"))){
               file_path <- file_list[!str_detect(file_list,regex("Transfers|Transfer|Archive|Archives",ignore_case = T))]
               data <- tibble(Path = str_extract(str_remove_all(file_path,"[:blank:]"),regex("(?<=Clinical_Affairs/).+|(?<=Upload Data/).+")),
@@ -43,15 +92,15 @@ server <- function(input, output) {
                                 str_extract(Path,regex("(?<=_[:digit:]{6}_[:digit:]{4})[:digit:]{2}")),sep = ":")),
                              Type = str_extract(Path,"AUU|UUU|GKS"),
                              Sensor = str_extract(Path,"[:alnum:]{2}(?=_ADC)"),
-                             Visit = str_extract(Path,regex("FV|V3|V4|V5|Visit3|Visit4|Visit5|Visit_3|Visit_4|Visit_5|C1|C2|C3|C4|Exit|Round1|Round2",ignore_case = T))) |> 
-                        distinct(pick(c(Site:Time)),.keep_all = T) |> 
+                             Visit = str_extract(Path,regex("FV|V3|V4|V5|Visit3|Visit4|Visit5|Visit_3|Visit_4|Visit_5|C1|C2|C3|C4|Exit|Round1|Round2",ignore_case = T))) |>
+                        distinct(pick(c(Site:Time)),.keep_all = T) |>
                         mutate(Count = n(),.by = c(Type,`Subject ID`,`Condition ID`,Visit))
-              # Count the most number of sensors  
-              most <- data |> 
-                      count(Sensor) |> 
-                      slice_max(n) |> 
-                      pull(Sensor)      
-            data <- data |>  
+              # Count the most number of sensors
+              most <- data |>
+                      count(Sensor) |>
+                      slice_max(n) |>
+                      pull(Sensor)
+            data <- data |>
                     mutate(Status = case_when(
                     str_length(`Subject ID`) != median(str_length(`Subject ID`),na.rm = T) ~ "No Good: Please Check Subject ID",
                     str_length(`Condition ID`) != 3 |
@@ -61,12 +110,12 @@ server <- function(input, output) {
                     str_extract(Site,"[:digit:]{3}") != str_sub(`Subject ID`,1,3) ~ "No Good: Put files on the wrong Site folders",
                     Sensor != most ~ "No Good: Wrong sensor type",
                     Count != 1 ~ "No Good: Duplicated Uploads",
-                    .default = "Good")) |> 
+                    .default = "Good")) |>
                   arrange(`Subject ID`)}
-          
+
           else {
-              file_path <- file_list[!str_detect(file_list,regex("Transfers|Transfer|Archive|Archives|Final",ignore_case = T))]
-          
+              file_path <- file_list[!str_detect(file_list,regex("Transfers|Transfer|Archive|Archives|Final|SDPT",ignore_case = T))]
+
               data <- tibble(Path = str_extract(str_remove_all(file_path,"[:blank:]"),regex("(?<=UploadData/).+")),
                              Site  = str_c(str_extract(Path,regex("(?<=DataFiles/)[:alnum:]+",ignore_case = T)),
                                          str_extract(Path,regex("(?<=DataFiles/[:alnum:]{3,4}[:punct:]{1})[:alnum:]+",ignore_case = T)),
@@ -79,9 +128,9 @@ server <- function(input, output) {
                                     str_extract(Path,regex("(?<=_[:digit:]{6}_[:digit:]{2})[:digit:]{2}")),
                                     str_extract(Path,regex("(?<=_[:digit:]{6}_[:digit:]{4})[:digit:]{2}")),sep = ":")),
                              Type = str_extract(Path,"[:alpha:]+"),
-                              Visit = str_extract(Path,regex("FV|V3|V4|V5|Visit3|Visit4|Visit5|Visit_3|Visit_4|Visit_5|C1|C2|C3|C4|Exit",ignore_case = T))) |>
+                              Visit = str_extract(Path,regex("FV|Interim|V3|V4|V5|Visit3|Visit4|Visit5|Visit_3|Visit_4|Visit_5|C1|C2|C3|C4|Exit",ignore_case = T))) |>
               # Remove dualsensors_events.csv and dualsensors_extracted_realm.zip
-                        distinct(pick(c(Site:Time)),.keep_all = T) |> 
+                        distinct(pick(c(Site:Time)),.keep_all = T) |>
                         mutate(Count = n(),.by = c(Type,`Subject ID`,`Condition ID`,Visit)) |>
                         mutate(Status = case_when(
                           str_length(`Subject ID`) != median(str_length(`Subject ID`),na.rm = T) ~ "No Good: Please Check Subject ID",
@@ -91,14 +140,16 @@ server <- function(input, output) {
                           is.na(`Subject ID`) | is.na(`Condition ID`) ~ "No Good: Please Check Subject ID and Condition ID",
                           str_extract(Site,"[:digit:]{3}") != str_sub(`Subject ID`,1,3) ~ "No Good: Put files on the wrong Site folders",
                           Count != 1 ~ "No Good: Duplicated Uploads",
-                          .default = "Good")) |> 
+                          .default = "Good")) |>
                         arrange(`Subject ID`)}})
-  
+
           output$table <- renderReactable({reactable(text(),paginationType = "jump", striped = TRUE, highlight = TRUE, searchable = TRUE, filterable = TRUE,defaultPageSize = 6, resizable = TRUE,defaultColDef = colDef(align = "center",minWidth = 115))})
-           
-      
+
+
 }
 
 shinyApp(ui, server)
+
+
 
 # https://stackoverflow.com/questions/73247832/download-data-into-excel-from-r-shiny-table-created-with-reactable
